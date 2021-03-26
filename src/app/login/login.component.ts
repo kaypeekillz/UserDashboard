0f7f8adffc55c72acfa89cfd2cdc383717eabecc
kaypeekillz <kehinde.onyekuwuluje@xputertechnologies.com>
2021-03-26 08:43:12 +0100

    more changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+
   }
 
   loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
   });
+
+  setUsername(e) {
+    this.username = e;
+  }
+
+  setPassword(e) {
+    this.password = e;
+  }
+
+  
+  
+  
 }
